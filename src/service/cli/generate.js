d43d75cd4377b1e4bcc2bd73f5eddeef072ba21f
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-19 01:19:46 +0300

    Add random post template

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const {TITLES, SENTENCES, CATEGORIES} = require(`../../constants`);
+
 const generatePosts = (count) => (
   Array(count).fill({}).map(() => ({
-    createdDate: Date.now()
+    title: TITLES[0],
+    announce: SENTENCES[0],
+    fullText: SENTENCES[1],
+    createdDate: Date.now(),
+    сategory: CATEGORIES,
   }))
 );
