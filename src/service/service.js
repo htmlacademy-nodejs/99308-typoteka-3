b8bd310dbd52d5e3559ff02df2ad6e738e60e1ba
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-19 07:17:43 +0300

    Add else block

--- src/service/service.js
+++ src/service/service.js
@@ -6,9 +6,9 @@ const {ExitCode, USER_ARGV_INDEX, DEFAULT_COMMAND} = require(`../constants`);
 const userArguments = process.argv.slice(USER_ARGV_INDEX);
 const [userCommand] = userArguments;
 
-if (userArguments.length === 0 || !Cli[userCommand]) {
+if (userArguments.length > 0 && Cli[userCommand]) {
+  Cli[userCommand].run(userArguments.slice(1));
+} else {
   Cli[DEFAULT_COMMAND].run();
   process.exit(ExitCode.SUCCESS);
 }
-
-Cli[userCommand].run(userArguments.slice(1));
