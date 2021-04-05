9a1d3d96f3de44fa4caef4561d86a550564d4005
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-06 00:11:53 +0300

    Change source for mock generation

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const {
-  TITLES,
-  SENTENCES,
-  CATEGORIES,
+  TITLES_FILE_PATH,
+  SENTENCES_FILE_PATH,
+  CATEGORIES_FILE_PATH,
   DEFAULT_COUNT,
   MAX_POSTS,
   FILE_NAME,
@@ -14,26 +14,39 @@ const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {getRandomInt, shuffle, getRandomDate} = require(`../../utils`);
 
-const generatePosts = (count) => (
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf-8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generatePosts = (count, titles, sentences, categories) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    announce: shuffle(SENTENCES).slice(0, getRandomInt(1, 4)).join(` `),
-    fullText: shuffle(SENTENCES).slice(0, getRandomInt(4, SENTENCES.length - 1)).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    announce: shuffle(sentences).slice(0, getRandomInt(1, 4)).join(` `),
+    fullText: shuffle(sentences).slice(0, getRandomInt(4, sentences.length - 1)).join(` `),
     createdDate: getRandomDate().toLocaleString(`ru`),
-    сategory: shuffle(CATEGORIES).slice(0, getRandomInt(1, 2)),
+    сategory: shuffle(categories).slice(0, getRandomInt(1, 2)),
   }))
 );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const titles = await readContent(TITLES_FILE_PATH);
+    const sentences = await readContent(SENTENCES_FILE_PATH);
+    const categories = await readContent(CATEGORIES_FILE_PATH);
     const [count] = args;
     const countPost = Number.parseInt(count, 10) || DEFAULT_COUNT;
     if (countPost > MAX_POSTS) {
       console.error(chalk.red(`Не больше ${MAX_POSTS} публикаций!`));
       process.exit(ExitCode.ERROR);
     }
-    const content = JSON.stringify(generatePosts(countPost));
+    const content = JSON.stringify(generatePosts(countPost, titles, sentences, categories));
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
