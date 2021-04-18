d54c83c61295bacdc74a9e62c00ef80ad048c21f
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-18 23:32:41 +0300

    Add main routes

--- src/express/index.js
+++ src/express/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require(`express`);
+const mainRouter = require(`./routes/main-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
+app.use(`/`, mainRouter);
 app.listen(DEFAULT_PORT);
