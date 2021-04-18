6db345e910822e91225b166ad404f25caa48836a
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-19 00:02:17 +0300

    Add articles routes

--- src/express/index.js
+++ src/express/index.js
@@ -3,10 +3,12 @@
 const express = require(`express`);
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
+const articlesRouter = require(`./routes/articles-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
+app.use(`/articles`, articlesRouter);
 app.listen(DEFAULT_PORT);
