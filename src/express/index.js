3ab152f526d5a95eb0f1b13a4ea95da6b215d9ad
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-05-17 19:21:32 +0300

    Add errors templates

--- src/express/index.js
+++ src/express/index.js
@@ -2,6 +2,7 @@
 
 const express = require(`express`);
 const path = require(`path`);
+const {HttpCode} = require(`../../src/constants`);
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
 const articlesRouter = require(`./routes/articles-routes`);
@@ -15,6 +16,8 @@ app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
+app.use((err, req, res, _next) => res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`));
+app.use((req, res) => res.status(HttpCode.NOT_FOUND).render(`errors/404`));
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
